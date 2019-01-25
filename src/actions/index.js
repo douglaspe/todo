21a8669257829d5e14fc2790cea5c56cb9a01a35
Douglas <douglas@gmail.com>
2019-01-25 09:59:17 -0200

    Iniciando com redux

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const CLICK_UPDATE_VALUE = 'CLICK_UPDATE_VALUE';
+
+export const clickButton = value => ({
+    type: 'CLICK_UPDATE_VALUE',
+    newValue: value
+  });
---
+++ src/reducers/clickReducer.js
@@ -0,0 +1,17 @@
+export const CLICK_UPDATE_VALUE = 'CLICK_UPDATE_VALUE';
+
+const initialState = {
+    newValue: 'Atualizado via Redux!'
+  };
+
+  export const clickReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'CLICK_UPDATE_VALUE':
+        return {
+          ...state,
+          newValue: action.newValue
+        };
+      default:
+        return state;
+    }
+  };
